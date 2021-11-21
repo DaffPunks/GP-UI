b96038257951a287efd0e34aa07225efd7313324
daff <daff@yandex-team.ru>
2021-11-21 16:28:09 +0300

    API: add Socket Events

---
+++ shared/constants.ts
@@ -0,0 +1,11 @@
+export enum ServerEvents {
+    StateUpdate = 'SERVER_STATE_UPDATE',
+    ChangeSrc = 'SERVER_CHANGE_SRC',
+}
+
+export enum ClientEvent {
+    ChangeSrc = 'SERVER_CHANGE_SRC',
+    Pause = 'CLIENT_PAUSE',
+    Play = 'CLIENT_PLAY',
+    Seek = 'CLIENT_SEEK',
+}
