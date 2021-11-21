963e7b445bb866eb119bef7083836b247d79c587
heymynameismilk <heymynameismilk@gmail.com>
2021-11-21 18:08:16 +0300

    player, sockets

--- api/src/socket.ts
+++ api/src/socket.ts
@@ -1,29 +1,34 @@
-import {ClientEvent, ServerEvent} from '../../ui/shared/constants';
-import {io} from './index';
-import store from './store'
+import { ClientEvent, ServerEvent } from "../../ui/src/shared/constants";
+import { io } from "./index";
+import store from "./store";
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.emit(ServerEvent.StateUpdate, store.get());
+  socket.emit(ServerEvent.StateUpdate, store.get());
 
-    socket.on(ClientEvent.ChangeSrc, (src) => {
-        store.set({src});
-        socket.emit(ServerEvent.ChangeSrc, store.get());
-    })
+  socket.on(ClientEvent.ChangeSrc, (src) => {
+    store.set({ src });
+    socket.emit(ServerEvent.ChangeSrc, store.get());
+  });
 
-    socket.on(ClientEvent.Pause, () => {
-        store.set({isPlaying: false});
-        socket.emit(ServerEvent.StateUpdate, store.get());
-    });
+  socket.on(ClientEvent.Pause, (timing) => {
+    store.set({ isPlaying: false, timing });
+    socket.broadcast.emit(ServerEvent.StateUpdate, store.get());
+  });
 
-    socket.on(ClientEvent.Play, () => {
-        store.set({isPlaying: true});
-        socket.emit(ServerEvent.StateUpdate, store.get());
-    });
+  socket.on(ClientEvent.Play, (timing) => {
+    store.set({ isPlaying: true, timing });
+    socket.broadcast.emit(ServerEvent.StateUpdate, store.get());
+  });
 
-    socket.on(ClientEvent.Seek, (timing) => {
-        store.set({timing});
-        socket.emit(ServerEvent.StateUpdate, store.get());
-    });
+  // socket.on(ClientEvent.Seek, (timing) => {
+  //   store.set({ timing });
+  //   socket.emit(ServerEvent.StateUpdate, store.get());
+  // });
+
+  socket.on(ClientEvent.StateUpdate, (state) => {
+    store.set(state);
+    socket.broadcast.emit(ServerEvent.StateUpdate, store.get());
+  });
 });
--- ui/src/components/Player/index.tsx
+++ ui/src/components/Player/index.tsx
@@ -1,19 +1,53 @@
-import bg from "../../assets/image.png";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Player.module.scss";
 import ReactPlayer from "react-player";
+import socket from "../../service/sockets";
+import { ClientEvent, ServerEvent } from "../../shared/constants";
 
 type Player = {
   link: string;
 };
 
 const Player = ({ link }: Player) => {
+  const [playing, setPlaying] = useState(false);
+
+  const ref = useRef<ReactPlayer>(null);
+
+  useEffect(() => {
+    socket.on(ServerEvent.StateUpdate, (data) => {
+      setPlaying(data.isPlaying);
+      if (ref.current) {
+        ref.current.seekTo(data.timing, "seconds");
+      }
+    });
+  }, []);
+
   return (
     <div className={styles.Player}>
       <ReactPlayer
+        ref={ref}
         width={"100%"}
         height={"100%"}
         controls={true}
+        playsinline
+        config={{
+          youtube: {
+            playerVars: { autoplay: 1, mute: 1 },
+          },
+        }}
+        playing={playing}
+        onPlay={() => {
+          if (!playing && ref.current) {
+            setPlaying(true);
+            socket.emit(ClientEvent.Play, ref.current.getCurrentTime());
+          }
+        }}
+        onPause={() => {
+          if (playing && ref.current) {
+            setPlaying(false);
+            socket.emit(ClientEvent.Pause, ref.current.getCurrentTime());
+          }
+        }}
         url={link || "https://www.youtube.com/watch?v=ysz5S6PUM-U"}
       />
     </div>
